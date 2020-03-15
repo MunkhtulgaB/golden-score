f651d8d7148616aea60e71ea1b7dd1261c340ad0
Munkhtulga B <munkhtulga.battogtokh@gmail.com>
2020-03-15 22:04:37 +0000

    Save optimisation state of tori_osotogari

--- vposer/optimize_pose.py
+++ vposer/optimize_pose.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 from human_body_prior.body_model.body_model_vposer import BodyModelWithPoser
-
+from scipy.ndimage import gaussian_filter
+from human_body_prior.tools.model_loader import load_vposer as poser_loader
 
 NUM_ITERS = 50
 MODEL_NAME = 'tori_osotogari'
@@ -12,26 +13,55 @@ MODEL_NAME = 'tori_osotogari'
 # Get the body model
 expr_dir = '../SMPL-X/vposer_v1_0' #'TRAINED_MODEL_DIRECTORY'  in this directory the trained model along with the model code exist
 bm_path =  '../SMPL-X/models/smplx/SMPLX_MALE.npz'#'PATH_TO_SMPLX_model.npz'  obtain from https://smpl-x.is.tue.mpg.de/downloads
+mano_exp_dir = expr_dir
+
 bm = BodyModelWithPoser(bm_path=bm_path, batch_size=1, poser_type='vposer', smpl_exp_dir=expr_dir)
 
 # Start from an approximation loaded from pickled SMPLify-x output
-body_model_path = 'input/source/' + MODEL_NAME + '/'
-body_pickle = open(body_model_path + '000.pkl', 'rb')
-body = pickle.load(body_pickle)
+try: 
+	body_model_path = 'results/' + MODEL_NAME + '/poZ_body.pkl'
+	print('Loading pose from:', body_model_path)
+	body_pickle = open(body_model_path, 'rb')
+	body = pickle.load(body_pickle)
+	pose_body = body.get('body_pose')
+except:
+	print('No existing pose found starting from original')
+	body_model_path = 'input/source/' + MODEL_NAME + '/'
+	body_pickle = open(body_model_path + '000.pkl', 'rb')
+	body = pickle.load(body_pickle)
 
-pose_body = body.get('body_pose')[0]
-poZ_body = torch.tensor(pose_body, requires_grad=True)
+	# hand left
+	bm.poser_handL_pt, bm.poser_handL_ps = poser_loader(mano_exp_dir)
+	bm.poser_handL_pt.to(bm.trans.device)
+
+	poZ_handL = bm.pose_hand.new(body.get('left_hand_pose'))
+	bm.register_parameter('poZ_handL', torch.nn.Parameter(poZ_handL, requires_grad=True))
+
+    # hand right
+	bm.poser_handR_pt, bm.poser_handR_ps = poser_loader(mano_exp_dir)
+	bm.poser_handR_pt.to(bm.trans.device)
+
+	poZ_handR = bm.pose_hand.new(body.get('right_hand_pose'))
+	bm.register_parameter('poZ_handR', torch.nn.Parameter(poZ_handR, requires_grad=True))
+	bm.pose_hand.requires_grad = False
+
+	pose_body = torch.tensor(body.get('body_pose')[0])
+
+
+random_modification = torch.empty(pose_body.shape).normal_(mean=1,std=0.01)
+poZ_body = torch.tensor(pose_body * random_modification, requires_grad=True)
 bm.register_parameter('poZ_body', torch.nn.Parameter(poZ_body, requires_grad=True))
 
 
 # Prepare to render
 target_img = pyredner.imread('input/targets/' + MODEL_NAME + '.png')
+# target_img = gaussian_filter(target_img, sigma=10)
 target = torch.tensor(target_img).double()
 pyredner.imwrite(target.cpu(), 'results/' + MODEL_NAME + '/target.png')
 
 img_dim = (target.shape[0], target.shape[1])
 
-cam_pos = torch.tensor([1., 1., 1.], requires_grad=True)
+cam_pos = torch.tensor([2.2, 0.2, -1.5], requires_grad=True)
 cam = pyredner.Camera(position = cam_pos,
                       look_at = torch.tensor([0.1, -0.4, 0.0]),
                       up = torch.tensor([0.0, 1.0, 0.0]),
@@ -48,17 +78,34 @@ materials = [mat_grey]
 
 
 shape_light = pyredner.Shape(\
-    vertices = torch.tensor([[-1.0, -1.0, 3.],
-                             [ 1.0, -1.0, 3.],
-                             [-1.0,  1.0, 3.],
-                             [ 1.0,  1.0, 3.]], device = pyredner.get_device()),
+    vertices = torch.tensor([[0.0, -1.0, -3.],
+                             [2.0, -1.0, -3.],
+                             [0.0,  1.0, -3.],
+                             [2.0,  1.0, -3.]], device = pyredner.get_device()),
     indices = torch.tensor([[0, 1, 2],[1, 3, 2]],
         dtype = torch.int32, device = pyredner.get_device()),
     uvs = None,
     normals = None,
     material_id = 0)
 
+shape_light1 = pyredner.Shape(\
+    vertices = torch.tensor([[-2.0, -1.0, -1.],
+                             [ 0.0, -1.0, -2.],
+                             [-2.0,  1.0, -1.],
+                             [ 0.0,  1.0, -2.]], device = pyredner.get_device()),
+    indices = torch.tensor([[0, 1, 2],[1, 3, 2]],
+        dtype = torch.int32, device = pyredner.get_device()),
+    uvs = None,
+    normals = None,
+    material_id = 0)
+
+
 light = pyredner.AreaLight(shape_id = 1, 
+                           intensity = torch.tensor([4.0,4.0,4.0]),
+                           two_sided=True,
+                           directly_visible=False)
+
+light1 = pyredner.AreaLight(shape_id = 2, 
                            intensity = torch.tensor([4.0,4.0,4.0]),
                            two_sided=True,
                            directly_visible=False)
@@ -71,20 +118,19 @@ shape_triangle = pyredner.Shape(
 	material_id=0)
 
 
-shapes = [shape_triangle, shape_light]
-area_lights = [light]
+shapes = [shape_triangle, shape_light, shape_light1]
+area_lights = [light, light1]
 scene = pyredner.Scene(cam, shapes, materials, area_lights)
 
-
 # Optimise
-optimizer = torch.optim.SGD([bm.poZ_body], lr=1, momentum=0.9, dampening=0.1)
-# cam_optimizer = torch.optim.Adam([cam_pos], lr=1e-3)
+# optimizer = torch.optim.SGD([bm.poZ_body], lr=1, momentum=0.9)
+optimizer = torch.optim.Adam([bm.poZ_body], lr=5e-2)
+orig_poZ = bm.poZ_body.clone()
 
 render = pyredner.RenderFunction.apply
 for t in range(NUM_ITERS):
     print('iteration:', t)
     optimizer.zero_grad()
-    # cam_optimizer.zero_grad()
     # Forward pass: render the image
 
     shape_triangle.vertices = bm.forward().v[0]
@@ -101,16 +147,22 @@ for t in range(NUM_ITERS):
     pyredner.imwrite(img.cpu(), 'results/' + MODEL_NAME + '/iter_{}.png'.format(t))
     # Compute the loss function. Here it is L2.
     loss = (img - target).pow(2).mean()
-    # loss += 0.1 * torch.norm(bm.poZ_body).double()      
+    # diff = orig_poZ.double() - bm.poZ_body.double()
+    # loss += (diff.pow(2).mean())
     loss.backward()
     print('Loss:', loss.item())
     # Take a gradient descent step.
     optimizer.step()
-    # cam_optimizer.step()
 
-result_obj = shapes[0]
-result_obj.vertices = result_obj.vertices.detach()
-pyredner.save_obj(result_obj, f'results/' + MODEL_NAME + '/result.obj')
+    # Pickle the poZ_body
+    pickle_out = open('results/' + MODEL_NAME + '/poZ_body.pkl',"wb")
+    pickle.dump(dict(body_pose=bm.poZ_body), pickle_out)
+    pickle_out.close()
+
+    # Save the resulting 3D model in .obj format
+    result_obj = shapes[0]
+    result_obj.vertices = result_obj.vertices.detach()
+    pyredner.save_obj(result_obj, f'results/' + MODEL_NAME + '/result.obj')
 
 # Save the images and differences.
 pyredner.imwrite(img.cpu(), 'results/' + MODEL_NAME + '/final.exr')
