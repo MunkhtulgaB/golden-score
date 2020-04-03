98d7292495d7328cab3c37fc45afc7ced5424ed2
Munkhtulga B <munkhtulga.battogtokh@gmail.com>
2020-04-03 14:34:26 +0100

    Add sources and license agreement

--- vposer/optimize_pose.py
+++ vposer/optimize_pose.py
@@ -1,3 +1,40 @@
+"""
+This file is based on the redner tutorial:
+https://github.com/BachiLi/redner/blob/master/tutorials/01_optimize_single_triangle.py
+I agree to their MIT License and reproduce it below:
+=====================================================================================
+MIT License
+Copyright (c) 2018 Tzu-Mao Li
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+=====================================================================================
+This file makes use of SMPLify-x: 
+
+@inproceedings{SMPL-X:2019,
+  title = {Expressive Body Capture: 3D Hands, Face, and Body from a Single Image},
+  author = {Pavlakos, Georgios and Choutas, Vasileios and Ghorbani, Nima and Bolkart, Timo and Osman, Ahmed A. A. and Tzionas, Dimitrios and Black, Michael J.},
+  booktitle = {Proceedings IEEE Conf. on Computer Vision and Pattern Recognition (CVPR)},
+  year = {2019}
+}
+
+By License agreement, I do not copy their Model & Software and the license. See more on:
+https://github.com/vchoutas/smplify-x/blob/master/LICENSE
+
+"""
+
 import pyredner
 import torch
 import matplotlib.pyplot as plt
@@ -8,7 +45,7 @@ from scipy.ndimage import gaussian_filter
 from human_body_prior.tools.model_loader import load_vposer as poser_loader
 
 NUM_ITERS = 50
-MODEL_NAME = 'tori_osotogari'
+MODEL_NAME = 'uke_osotogari_filled'
 
 # Get the body model
 expr_dir = '../SMPL-X/vposer_v1_0' #'TRAINED_MODEL_DIRECTORY'  in this directory the trained model along with the model code exist
@@ -57,13 +94,14 @@ bm.register_parameter('poZ_body', torch.nn.Parameter(poZ_body, requires_grad=Tru
 target_img = pyredner.imread('input/targets/' + MODEL_NAME + '.png')
 # target_img = gaussian_filter(target_img, sigma=10)
 target = torch.tensor(target_img).double()
+target *= 20
 pyredner.imwrite(target.cpu(), 'results/' + MODEL_NAME + '/target.png')
 
 img_dim = (target.shape[0], target.shape[1])
 
-cam_pos = torch.tensor([2.2, 0.2, -1.5], requires_grad=True)
+cam_pos = torch.tensor([2.0, -0.3, 1.3], requires_grad=True)
 cam = pyredner.Camera(position = cam_pos,
-                      look_at = torch.tensor([0.1, -0.4, 0.0]),
+                      look_at = torch.tensor([-0.3, -0.6, 0.0]),
                       up = torch.tensor([0.0, 1.0, 0.0]),
                       fov = torch.tensor([45.0]), # in degree
                       clip_near = 1e-2, # needs to > 0
@@ -78,10 +116,10 @@ materials = [mat_grey]
 
 
 shape_light = pyredner.Shape(\
-    vertices = torch.tensor([[0.0, -1.0, -3.],
-                             [2.0, -1.0, -3.],
-                             [0.0,  1.0, -3.],
-                             [2.0,  1.0, -3.]], device = pyredner.get_device()),
+    vertices = torch.tensor([[3.0, -1.0, -1.],
+                             [3.0, -1.0, 2.],
+                             [3.0,  1.0, -1.],
+                             [3.0,  1.0, 2.]], device = pyredner.get_device()),
     indices = torch.tensor([[0, 1, 2],[1, 3, 2]],
         dtype = torch.int32, device = pyredner.get_device()),
     uvs = None,
@@ -89,10 +127,10 @@ shape_light = pyredner.Shape(\
     material_id = 0)
 
 shape_light1 = pyredner.Shape(\
-    vertices = torch.tensor([[-2.0, -1.0, -1.],
-                             [ 0.0, -1.0, -2.],
-                             [-2.0,  1.0, -1.],
-                             [ 0.0,  1.0, -2.]], device = pyredner.get_device()),
+    vertices = torch.tensor([[-2.0, -1.0, -4.],
+                             [ 0.0, -1.0, -4.],
+                             [-2.0,  1.0, -4.],
+                             [ 0.0,  1.0, -4.]], device = pyredner.get_device()),
     indices = torch.tensor([[0, 1, 2],[1, 3, 2]],
         dtype = torch.int32, device = pyredner.get_device()),
     uvs = None,
@@ -101,7 +139,7 @@ shape_light1 = pyredner.Shape(\
 
 
 light = pyredner.AreaLight(shape_id = 1, 
-                           intensity = torch.tensor([4.0,4.0,4.0]),
+                           intensity = torch.tensor([6.0,6.0,6.0]),
                            two_sided=True,
                            directly_visible=False)
 
@@ -125,6 +163,7 @@ scene = pyredner.Scene(cam, shapes, materials, area_lights)
 # Optimise
 # optimizer = torch.optim.SGD([bm.poZ_body], lr=1, momentum=0.9)
 optimizer = torch.optim.Adam([bm.poZ_body], lr=5e-2)
+# optimizer = torch.optim.Adam([cam.position], lr=1e-2)
 orig_poZ = bm.poZ_body.clone()
 
 render = pyredner.RenderFunction.apply
