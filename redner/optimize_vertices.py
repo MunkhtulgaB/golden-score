76898ee74f9856ca4ff723f0e674586d8c82f767
Munkhtulga B <munkhtulga.battogtokh@gmail.com>
2020-04-03 14:28:55 +0100

    Add license agreement and base file

--- redner/optimize_vertices.py
+++ redner/optimize_vertices.py
@@ -1,3 +1,35 @@
+"""
+This file is based on the redner tutorial:
+https://github.com/BachiLi/redner/blob/master/tutorials/01_optimize_single_triangle.py
+
+I agree to their MIT License and reproduce it below:
+=====================================================================================
+
+MIT License
+
+Copyright (c) 2018 Tzu-Mao Li
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+=====================================================================================
+"""
+
+
 # The Python interface of redner is defined in the pyredner package
 import pyredner
 import torch
