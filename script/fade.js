11b124014464f05b1aade012d3afbd05a73e5a40
mindscapes-project <mindscapes.kcl@gmail.com>
2022-07-17 13:44:44 +0100

    Add display title to webpage

---
+++ script/fade.js
@@ -0,0 +1,14 @@
+$(window).scroll(function() {
+
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+
+    $(".fade").each(function() {
+        var objectStart = $(this).offset().top + 100; // $(this).outerHeight();
+
+        if (objectStart < windowBottom) { //object comes into view (scrolling down)
+            if ($(this).css("opacity")==0) {$(this).fadeTo(800,1);}
+        } else { //object goes out of view (scrolling up)
+            if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        }
+    })
+})
