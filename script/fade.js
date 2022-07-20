6b56c54d5d5aacb7c71a6378a705ce3f18d3d7a4
mindscapes-project <mindscapes.kcl@gmail.com>
2022-07-20 09:45:27 +0100

    Finish first full version

--- script/fade.js
+++ script/fade.js
@@ -3,7 +3,7 @@ $(window).scroll(function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
 
     $(".fade").each(function() {
-        var objectStart = $(this).offset().top + 100; // $(this).outerHeight();
+        var objectStart = $(this).offset().top + 300; // $(this).outerHeight();
 
         if (objectStart < windowBottom) { //object comes into view (scrolling down)
             if ($(this).css("opacity")==0) {$(this).fadeTo(800,1);}
